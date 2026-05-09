54c8ef0e5eee833d82bd3b3db675216805098a03
chenxingguo43-sudo <user@example.com>
2026-05-10 03:14:42 +0800

    Enable cloud sync on Pages

---
+++ cloudsync-config.js
@@ -0,0 +1,4 @@
+// CloudSync configuration
+// Place your GitHub personal access token here (needs 'gist' scope)
+var CLOUDSYNC_GIST_ID = 'ab24c65438826cb098f795e1903f4095';
+var CLOUDSYNC_TOKEN = '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';
